0baf340069c1d2ec56a81d0cd7055bfda69db34b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-31 13:49:25 +0700

    Xong giao diện màn hình Start

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -11,9 +11,70 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  int getNumberOfAnimalTypes() {
+    switch (widget.level) {
+      case "easy":
+        return 4;
+      case "medium":
+        return 6;
+      case "hard":
+        return 12;
+      default:
+        return 4;
+    }
+  }
+
+  int getTotalTime() {
+    switch (widget.level) {
+      case "easy":
+        return 150;
+      case "medium":
+        return 100;
+      case "hard":
+        return 50;
+      default:
+        return 50;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     log("StartPage: ${widget.level}");
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Ready?"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              "Level: ${widget.level.toUpperCase()}",
+              style: const TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Animal types: ${getNumberOfAnimalTypes()}",
+              style: const TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Total time: ${getTotalTime()} seconds",
+              style: const TextStyle(fontSize: 30),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    fixedSize: const Size(200, 50)),
+                child: const Text(
+                  'Start',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+          ],
+        ),
+      )
+    );
   }
 }
