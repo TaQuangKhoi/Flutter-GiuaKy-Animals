627ffa307903f46f155ca3e39587dbbb971fd7b4
Tạ Quang Khôi <khoi0941@gmail.com>
2023-07-31 14:00:51 +0700

    Format code Start Page

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,9 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+
+import 'game_page.dart';
 
 class StartPage extends StatefulWidget {
   final String level;
+
   const StartPage({super.key, required this.level});
 
   @override
@@ -40,40 +41,42 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Ready?"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text(
-              "Level: ${widget.level.toUpperCase()}",
-              style: const TextStyle(fontSize: 30),
-            ),
-            Text(
-              "Animal types: ${getNumberOfAnimalTypes()}",
-              style: const TextStyle(fontSize: 30),
-            ),
-            Text(
-              "Total time: ${getTotalTime()} seconds",
-              style: const TextStyle(fontSize: 30),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    fixedSize: const Size(200, 50)),
-                child: const Text(
-                  'Start',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                )),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Ready?"),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                "Level: ${widget.level.toUpperCase()}",
+                style: const TextStyle(fontSize: 30),
+              ),
+              Text(
+                "Animal types: ${getNumberOfAnimalTypes()}",
+                style: const TextStyle(fontSize: 30),
+              ),
+              Text(
+                "Total time: ${getTotalTime()} seconds",
+                style: const TextStyle(fontSize: 30),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GamePage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      fixedSize: const Size(200, 50)),
+                  child: const Text(
+                    'Start',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  )),
+            ],
+          ),
+        ));
   }
 }
